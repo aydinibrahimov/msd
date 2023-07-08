48070d415d736fe3beba59f693912bcb20c7f023
Admin <aydin.ibrahimov.a@asoiu.edu.az>
2023-07-08 20:07:05 +0400

    frontend v-0.1.6

---
+++ msd/src/main/resources/templates/javascript/app.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    function showNavbar(toggleId, navId, bodyId, headerId) {
+        const toggle = $('#' + toggleId);
+        const nav = $('#' + navId);
+        const bodypd = $('#' + bodyId);
+        const headerpd = $('#' + headerId);
+
+        if (toggle.length && nav.length && bodypd.length && headerpd.length) {
+            toggle.on('click', function() {
+                nav.toggleClass('show');
+                toggle.toggleClass('bx-x');
+                bodypd.toggleClass('body-pd');
+                headerpd.toggleClass('body-pd');
+            });
+        }
+    }
+
+    showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header');
+
+    const linkColor = $('.nav_link');
+
+    function colorLink() {
+        if (linkColor.length) {
+            linkColor.removeClass('active');
+            $(this).addClass('active');
+        }
+    }
+
+    linkColor.on('click', colorLink);
+
+    // Initialize DataTable
+    $('#data-table').DataTable();
+});
