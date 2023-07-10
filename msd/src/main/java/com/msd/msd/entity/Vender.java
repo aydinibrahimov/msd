79227f75c6a560d65f02da350d1c6229fa9a1b95
Admin <aydininf@gmail.com>
2023-07-10 10:51:28 +0400

    Entity

---
+++ msd/src/main/java/com/msd/msd/entity/Vender.java
@@ -0,0 +1,16 @@
+package com.msd.msd.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "vender")
+public class Vender {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private  String name;
+    private Date birthDate;
+}
