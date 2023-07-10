0243025990d70d700b92feaf7e6f0da10419a70f
Admin <aydininf@gmail.com>
2023-07-10 10:41:04 +0400

    Entity

--- msd/src/main/java/com/msd/msd/entity/Product.java
+++ msd/src/main/java/com/msd/msd/entity/Product.java
@@ -1,18 +1,14 @@
 package com.msd.msd.entity;
 
-
 import jakarta.persistence.*;
-import lombok.Data;
 
 @Entity
-@Table(name = "client")
-@Data
-public class Client {
+@Table(name = "product")
+public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private  String name;
-
-
+    private String description;
 }
