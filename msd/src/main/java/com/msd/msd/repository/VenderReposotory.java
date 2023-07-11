2fbf6a682ac4218b22628fe272537306aacf59e4
Admin <aydininf@gmail.com>
2023-07-11 12:44:20 +0400

    JPA repository

---
+++ msd/src/main/java/com/msd/msd/repository/VenderReposotory.java
@@ -0,0 +1,7 @@
+package com.msd.msd.repository;
+
+import com.msd.msd.entity.Vender;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VenderReposotory extends JpaRepository<Vender,Long> {
+}
