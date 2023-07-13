42011e2a28fbd671287917b3b5c454844f223249
Admin <aydininf@gmail.com>
2023-07-13 23:09:31 +0400

    Invoice Service

---
+++ msd/src/main/java/com/msd/msd/service/InvoiceService.java
@@ -0,0 +1,50 @@
+package com.msd.msd.service;
+
+import com.msd.msd.entity.Invoice;
+import com.msd.msd.repository.InvoiceRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class InvoiceService {
+    private final InvoiceRepository invoiceRepository;
+
+
+    public Invoice addInvoice(Invoice invoice) {
+        return invoiceRepository.save(invoice);
+    }
+
+    public Invoice updateInvoiceById(Invoice invoice, Long id) {
+        Optional<Invoice> optionalInvoice = invoiceRepository.findById(id);
+        if (optionalInvoice.isPresent()) {
+            return invoiceRepository.save(invoice);
+        } else {
+            return optionalInvoice
+                    .orElseThrow(() -> new RuntimeException("It failed to find the invoice with the ID of " + id));
+        }
+    }
+
+    public void deleteInvoiceById(Long id) {
+        Optional<Invoice> optionalInvoice = invoiceRepository.findById(id);
+        if (optionalInvoice.isPresent()) {
+            invoiceRepository.deleteById(id);
+        } else {
+            optionalInvoice
+                    .orElseThrow(() -> new RuntimeException("It failed to find the invoice with the ID of " + id));
+        }
+    }
+
+    public Invoice getInvoiceById(Long id) {
+        return invoiceRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("It failed to find the invoice with the ID of " + id))
+    }
+
+    public List<Invoice> getAllInvoices() {
+        return invoiceRepository.findAll();
+    }
+
+}
