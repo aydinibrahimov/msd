85e6cc0d5bbb51f2f4445fa12594025172ef2761
Admin <aydininf@gmail.com>
2023-07-14 15:18:28 +0400

    types

---
+++ msd/src/main/java/com/msd/msd/entity/Dictionary.java
@@ -0,0 +1,21 @@
+package com.msd.msd.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+
+@Entity
+@Data
+@ToString
+@Table(name = "dictionary")
+public class Dictionary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fullName;
+
+@ManyToOne
+@JoinColumn(name = "dict_type_id")
+private DictionaryType dictionaryType;
+}
---
+++ msd/src/main/java/com/msd/msd/entity/DictionaryType.java
@@ -0,0 +1,21 @@
+package com.msd.msd.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.List;
+
+@Entity
+@Data
+@ToString
+@Table(name = "dict_type")
+public class DictionaryType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "dictionaryType")
+    private List<Dictionary> dictionaries;
+}
