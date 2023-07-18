f6e4c9a444ee310f6c0905ae262e3fe71fc99b04
Admin <aydininf@gmail.com>
2023-07-18 22:14:59 +0400

    logging

--- msd/src/main/java/com/msd/msd/enums/ClientErrorCode.java
+++ msd/src/main/java/com/msd/msd/enums/ClientErrorCode.java
@@ -6,7 +6,13 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ClientErrorCode {
-    CLIENT_NOT_FOUND(101,"Client with id was not found");
+
+    UNKNOWN_ERROR(100, "Unknown error"),
+    CLIENT_NOT_FOUND(101, "Client with id was not found"),
+    NOT_VALID_VALUE(102," would not be null"),
+    ACCESS_DENIED(103, " access denied"),
+    ;
+
 
     private final int code;
     private final String message;
