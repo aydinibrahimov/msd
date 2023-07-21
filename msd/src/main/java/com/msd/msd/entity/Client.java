2c7dedfd66bf720a25445d39df4dcfcfcb2ff094
Admin <aydininf@gmail.com>
2023-07-21 22:36:23 +0400

    Client Auth

--- msd/src/main/java/com/msd/msd/entity/Client.java
+++ msd/src/main/java/com/msd/msd/entity/Client.java
@@ -2,15 +2,22 @@ package com.msd.msd.entity;
 
 
 import jakarta.persistence.*;
+import jakarta.security.auth.message.AuthException;
+import jakarta.security.auth.message.MessageInfo;
+import jakarta.security.auth.message.config.ClientAuthConfig;
+import jakarta.security.auth.message.config.ClientAuthContext;
 import lombok.Data;
 import lombok.ToString;
+import org.springframework.security.core.userdetails.User;
 
+import javax.security.auth.Subject;
 import java.util.List;
+import java.util.Map;
 
 @Entity
 @Table(name = "client")
 @Data
-public class Client {
+public class Client implements ClientAuthConfig {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,4 +32,33 @@ public class Client {
     List<Invoice> invoices;
 
 
+    @Override
+    public ClientAuthContext getAuthContext(String s, Subject subject, Map<String, Object> map) throws AuthException {
+        return null;
+    }
+
+    @Override
+    public String getMessageLayer() {
+        return null;
+    }
+
+    @Override
+    public String getAppContext() {
+        return null;
+    }
+
+    @Override
+    public String getAuthContextID(MessageInfo messageInfo) {
+        return null;
+    }
+
+    @Override
+    public void refresh() {
+
+    }
+
+    @Override
+    public boolean isProtected() {
+        return false;
+    }
 }
