13391357d0d636906f535b8d8f10963deef6cbe0
Admin <aydininf@gmail.com>
2023-07-30 14:25:16 +0400

    30.07.23

--- msd/src/main/java/com/msd/msd/service/InvoiceService.java
+++ msd/src/main/java/com/msd/msd/service/InvoiceService.java
@@ -1,6 +1,8 @@
 package com.msd.msd.service;
 
 import com.msd.msd.entity.Invoice;
+import com.msd.msd.enums.CustomErrorCode;
+import com.msd.msd.exception.CustomException;
 import com.msd.msd.repository.InvoiceRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -24,7 +26,7 @@ public class InvoiceService {
             return invoiceRepository.save(invoice);
         } else {
             return optionalInvoice
-                    .orElseThrow(() -> new RuntimeException("It failed to find the invoice with the ID of " + id));
+                    .orElseThrow(() ->  new CustomException(CustomErrorCode.UNKNOWN_ERROR));
         }
     }
 
@@ -34,13 +36,13 @@ public class InvoiceService {
             invoiceRepository.deleteById(id);
         } else {
             optionalInvoice
-                    .orElseThrow(() -> new RuntimeException("It failed to find the invoice with the ID of " + id));
+                    .orElseThrow(() ->  new CustomException(CustomErrorCode.UNKNOWN_ERROR));
         }
     }
 
     public Invoice getInvoiceById(Long id) {
         return invoiceRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("It failed to find the invoice with the ID of " + id));
+                .orElseThrow(() ->  new CustomException(CustomErrorCode.UNKNOWN_ERROR));
     }
 
     public List<Invoice> getAllInvoices() {
