1a0e1e5df3c7f3ddbef9ddc6a42e9d89b113c942
Admin <aydininf@gmail.com>
2023-07-30 13:36:02 +0400

    30.07.23

--- msd/src/main/java/com/msd/msd/service/VendorService.java
+++ msd/src/main/java/com/msd/msd/service/VendorService.java
@@ -5,7 +5,10 @@ import com.msd.msd.entity.Vendor;
 import com.msd.msd.enums.CustomErrorCode;
 import com.msd.msd.exception.CustomException;
 import com.msd.msd.repository.VendorReposotory;
+import com.msd.msd.rest.model.dto.VendorDTO;
+import com.msd.msd.rest.model.response.VendorResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -54,5 +57,19 @@ public class VendorService {
         return vendorReposotory.findById(id)
                 .orElseThrow(() -> new CustomException(CustomErrorCode.VENDOR_NOT_FOUND));
     }
+
+    private VendorDTO convertToVendorDTO(Vendor vendor) {
+        VendorDTO vendorDTO = new VendorDTO();
+        BeanUtils.copyProperties(vendor, vendorDTO);
+        return vendorDTO;
+    }
+
+    private VendorResponse makeVendorResponse(List<VendorDTO> vendorDTOList) {
+        return VendorResponse.builder()
+                .vendors(vendorDTOList)
+                .build();
+
+    }
+
 
 }
