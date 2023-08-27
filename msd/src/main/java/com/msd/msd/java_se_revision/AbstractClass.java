7e7442e194eb91f0a94f012ad3c3ba11bc34fb42
Admin <aydininf@gmail.com>
2023-08-27 17:59:50 +0400

    just repetition

--- msd/src/main/java/com/msd/msd/java_se_revision/AbstractClass.java
+++ msd/src/main/java/com/msd/msd/java_se_revision/AbstractClass.java
@@ -2,7 +2,14 @@ package com.msd.msd.java_se_revision;
 
 public abstract class AbstractClass {
 
+
     public static void main(String[] args) {
-        System.out.println("Main method in abstract class ");
+        String a = "c";
+        String b = "c";
+        String c = new String("c");
+        System.out.println("a " + a instanceofb);
+        System.out.println("b " + c.equals(b));
+        System.out.println("s " + c.equals(a));
+
     }
 }
--- msd/src/main/java/com/msd/msd/java_se_revision/Common.java
+++ msd/src/main/java/com/msd/msd/java_se_revision/Common.java
@@ -3,6 +3,8 @@ package com.msd.msd.java_se_revision;
 public class Common implements InterfaceB {
     public static void main(String[] args) {
 
+        System.out.println( instanceof InterfaceA);
+
     }
 
 
