e47c29002555a06189ea42cc9907a76b74341740
Admin <aydininf@gmail.com>
2023-08-27 16:31:09 +0400

    just repetition

---
+++ msd/src/main/java/com/msd/msd/java_se_revision/AbstractClass.java
@@ -0,0 +1,8 @@
+package com.msd.msd.java_se_revision;
+
+public abstract class AbstractClass {
+
+    public static void main(String[] args) {
+        System.out.println("Main method in abstract class ");
+    }
+}
