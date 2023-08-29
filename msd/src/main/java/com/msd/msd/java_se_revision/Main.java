8e42302dfedba290f6980347eff17ca35911d346
Admin <aydininf@gmail.com>
2023-08-29 21:50:59 +0400

    just repetition

--- msd/src/main/java/com/msd/msd/java_se_revision/Main.java
+++ msd/src/main/java/com/msd/msd/java_se_revision/Main.java
@@ -1,17 +1,11 @@
 package com.msd.msd.java_se_revision;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Set<Integer > set=new TreeSet<>();
-        set.add(1);
-        set.add(0);
+        Map<String,String>=new HashMap<>();
 
 
-        set.forEach((sets)-> System.out.println(sets));
     }
 }
