897a7282dae31382886daa77ff978c99bad5284c
Admin <aydininf@gmail.com>
2023-08-31 18:28:41 +0400

    just repetition

--- msd/src/main/java/com/msd/msd/java_se_revision/Cava.java
+++ msd/src/main/java/com/msd/msd/java_se_revision/Cava.java
@@ -11,6 +11,22 @@ public class Cava {
 
     }
 
+
+    public static int hourglassSum(List<List<Integer>> arr) {
+        int rows=arr.length;
+        int columns=arr[0].length;
+        int maxHourglassSum=Integer.MIN_VALUE;
+        for(int i=0;i<rows-2;i++){
+            for(int j=0;i<columns-2;j++){
+
+                int  currentSum=arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+
+                        arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
+                maxHourglassSum=Math.max(maxHourglassSum,currentSum);
+            }
+
+        }
+        return maxHourglassSum;
+    }
     public static String isBracketsClosed(String brackets ) {
         Stack<String> stack = new Stack<>();
         stack.push(brackets);
