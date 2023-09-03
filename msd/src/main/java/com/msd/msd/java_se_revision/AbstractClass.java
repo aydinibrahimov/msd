f9b8c0e351b871b72cd551d02e2cba4291ef0a5e
Admin <aydininf@gmail.com>
2023-09-03 14:46:11 +0400

    just repetition

--- msd/src/main/java/com/msd/msd/java_se_revision/AbstractClass.java
+++
@@ -1,7 +0,0 @@
-package com.msd.msd.java_se_revision;
-
-public abstract class AbstractClass {
-
-
-
-}
--- msd/src/main/java/com/msd/msd/java_se_revision/InterfaceB.java
+++
@@ -1,5 +0,0 @@
-package com.msd.msd.java_se_revision;
-
-public interface InterfaceB  {
-    void show();
-}
